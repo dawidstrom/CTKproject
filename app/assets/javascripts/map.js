11f3aacbe43768375d13e6fd6377b9b761e1bb72
David Ström <Dawid.Strom@gmail.com>
2018-03-09 12:07:22 +0100

    Changed AJAX approach to use form helpers (still not working properly). Fixed so getWeather action can handle both coordinates and city,country queries. Swiched so weather API is used instead of downloaded json files.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -8,38 +8,82 @@
 var start_x = 57.71;
 var start_y = 11.98;
 
+// Placeholder text for city weather query.
 var prev_placeholder = "<City>,<Country Abbreviation>";
 var new_placeholder = "<Longitude>,<Latitude>";
+
+
+// Wait for the document (and JQuery) to become ready.
+window.onload = function() {
+	// Set CSRF header in all ajax requests.
+	$.ajaxSetup({
+		headers: {
+			'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+		}
+	});
+}
+
 
 // Periodically change the placeholder text in the city query textfield.
 setInterval(function() {
 	document.getElementById('city_query').placeholder = new_placeholder;
+
+	// Swap the placeholder values.
 	var tmp = new_placeholder;
 	new_placeholder = prev_placeholder;
 	prev_placeholder = tmp;
 }, 3000);
 
+
 // Sets the element with id "map" to be a Google Maps map, with position x and
 // y.
 function setMapByCoordinates(x, y) { 
+	// The current weather information for selected city.
+	var weather_info = '<h3>Weather Information not present at this time.</h3>';
+
+	// Setting the map to view the current city.
 	var uluru = { lat: x, lng: y};
-	var map = new google.maps.Map(document.getElementById('map'), { zoom: 7, center: uluru }); 
+	var map = new google.maps.Map(document.getElementById('map'), { zoom: 10, center: uluru }); 
 	var marker = new google.maps.Marker({ position: uluru, map: map }); 
+
+	// Adds a info window to the marker, displaying weather info.
+	var info_window = new google.maps.InfoWindow({
+		content: weather_info
+	});
+
+	// Open window on click.
+	marker.addListener('click', function() {
+		info_window.open(map, marker);
+	});
 }
+
 
 // Set the element with id "map" be a Google Maps map, with position based on
 // city name and country abbreviation.
 function setMapByCityAndCountry(city, country) {
-	// Todo
-	alert('This should set map by city and country');
+	$.post({
+		url: "/map/test",
+		type: "post",
+		data: { city:city, country:country },
+		success: function() {
+			// Replace weather data and map with those received from server call.
+			alert('success');
+		},
+		error: function() {
+			alert('error occured when setting map by city and country');
+		}
+	});
 }
+
 
 // Makes the Enter key trigger a click.
 function updateMapOnEnter(key) {
+	// Enter has keyCode 13.
 	if (key == 13) {
 		document.getElementById('city_query_submit').click();
 	}
 }
+
 
 // Update map with input.
 function updateMapWithInput() {
@@ -65,12 +109,13 @@ function updateMapWithInput() {
 	}
 }
 
+
+// Sets the starting map.
+function startMap() {
+	setMapByCoordinates(start_x, start_y);
+}
+
 // Helper function to test if some variable is a number.
 function isNumber(val) {
 	return !isNaN(parseFloat(val)) && isFinite(val);
-}
-
-// Initializes 
-function startMap() {
-	setMapByCoordinates(start_x, start_y);
 }
--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,10 +4,10 @@ class MapController < ApplicationController
   def index
 	  # Get the API key for Google Maps Javascript API.
 	  gmapkey = File.read("#{Rails.root}/app/assets/gmapsapi.key")
-	  @map = "https://maps.googleapis.com/maps/api/js?key=#{gmapkey}&callback=startMap"
 
 	  # Get the API key for OpenWeatherAPI.
 	  openweatherkey = File.read("#{Rails.root}/app/assets/openweatherapi.key")
+	  @map = "https://maps.googleapis.com/maps/api/js?key=#{gmapkey}&callback=startMap"
 
 	  # Get the cities to check polution and weather data for.
 	  cities = File.read("#{Rails.root}/app/assets/cities")
@@ -17,6 +17,20 @@ class MapController < ApplicationController
 	  # Use downloaded json files to not excessivly tax the API system.
 	  @weather = Hash.new(cities.size)
 	  cities.each do |city|
+
+		  # Get weather information from weather api for city.
+		  weather_info = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&APPID="+openweatherkey
+		  url = URI.parse(weather_info)
+		  req = Net::HTTP::Get.new(url.to_s)
+		  res = Net::HTTP.start(url.host, url.port) {|http|
+			  http.request(req)
+		  }
+
+		  # Save and parse the result of the query.
+		  res = res.body
+		  res = JSON.parse res
+
+=begin    Used to avoid excessive API calls during development.
 		  # Format city so it is easier to get the downloaded json files.
 		  city_formatted = city.dup
 		  city_formatted.gsub! ' ','_'
@@ -25,33 +39,90 @@ class MapController < ApplicationController
 		  # Retrieve the json file for this particular city and parse it.
 		  res = File.read("#{Rails.root}/app/assets/"+city_formatted[0]+".json")
 		  res = JSON.parse res
+=end
 
-		  arr = Array.new(3)
+		  # Gather weather information.
+		  weather_info = Array.new(5)
 		  # Retrieve weather information.
-		  arr[0] = res['list'][0]['weather'][0]['main']
+		  weather_info[0] = res['list'][0]['weather'][0]['main']
 		  # Retrieve temperature and convert it from Kelvin to Celsius, round
 		  # the result to nearest two decimals.
-		  arr[1] = (res['list'][0]['main']['temp']-273.15).round(2)
+		  weather_info[1] = (res['list'][0]['main']['temp']-273.15).round(2)
 		  # Retrieve wind speed.
-		  arr[2] = res['list'][0]['wind']['speed']
+		  weather_info[2] = res['list'][0]['wind']['speed']
+		  # Extract lat, long coordinates for city.
+		  weather_info[3] = res['city']['coord']['lat'].round(3)
+		  weather_info[4] = res['city']['coord']['lon'].round(3)
 
 		  # Make the retrieved information accessible to the view.
-		  @weather[city] = arr
+		  @weather[city] = weather_info
+	  end
+  end
+
+  def getWeatherInfo
+	  query = params[:city_query]
+	  parsed_query = query.split(',')
+
+	  # Get the API key for Google Maps Javascript API.
+	  gmapkey = File.read("#{Rails.root}/app/assets/gmapsapi.key")
+
+	  # Get the API key for OpenWeatherAPI.
+	  openweatherkey = File.read("#{Rails.root}/app/assets/openweatherapi.key")
+
+	  # Safely checks if the provided query is numeric or not.
+	  isNumerics = Float(parsed_query[0]) != nil and Float(parsed_query[1]) != nil rescue false
+
+	  # Determine if coordinates or city,country abbreviation should be used for 
+	  # API request.
+	  if isNumerics
+	  	openweather_url = "http://api.openweathermap.org/data/2.5/forecast?lat=#{parsed_query[0]}&lon=#{parsed_query[1]}&APPID="+openweatherkey
+	  else
+	  	openweather_url = "http://api.openweathermap.org/data/2.5/forecast?q=#{query}&APPID="+openweatherkey
 	  end
 
-=begin
-	  # Don't perform excessive API calls during development.
-	  @cities.each do |city|
-		  str = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&APPID="+openweatherkey
-		  url = URI.parse(str)
-		  req = Net::HTTP::Get.new(url.to_s)
-		  res = Net::HTTP.start(url.host, url.port) {|http|
-			  http.request(req)
-		  }
-		  res = res.body
-		  res = JSON.parse res
-		  @data[city] = res['list'][0]['weather'][0]['main']
+	  #@map = "https://maps.googleapis.com/maps/api/js?key=#{gmapkey}&callback=startMap"
+
+	  # Retrieve weather information about coordinate or city.
+	  url = URI.parse(openweather_url)
+	  req = Net::HTTP::Get.new(url.to_s)
+	  res = Net::HTTP.start(url.host, url.port) {|http|
+		  http.request(req)
+	  }
+
+	  puts openweather_url
+
+	  # Save and parse the query result.
+	  res = res.body
+	  res = JSON.parse res
+
+	  weather_info = Array.new(5)
+	  # Retrieve weather information.
+	  weather_info[0] = res['list'][0]['weather'][0]['main']
+	  # Retrieve temperature and convert it from Kelvin to Celsius, round
+	  # the result to nearest two decimals.
+	  weather_info[1] = (res['list'][0]['main']['temp']-273.15).round(2)
+	  # Retrieve wind speed.
+	  weather_info[2] = res['list'][0]['wind']['speed']
+	  # Extract lat, long coordinates for city. Round to 3 since openweatherapi
+	  # can't process requests with more than 3 decimals for coordinates.
+	  weather_info[3] = res['city']['coord']['lat'].round(3)
+	  weather_info[4] = res['city']['coord']['lon'].round(3)
+
+	  # Combine the city name and weather information.
+	  weather = Hash.new(1)
+
+	  city = res['city']['name']+','+res['city']['country']
+	  weather[city] = weather_info
+
+	  puts weather
+
+	  # Respond to AJAX with json of the hashed information.
+	  # Currently not working correctly.
+	  respond_to do |format|
+		  format.json do
+			  render json: weather
+		  end
 	  end
-=end
   end
+
 end
