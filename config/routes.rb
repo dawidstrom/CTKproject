110a0e23f4cc7bdfdfdd0789d20695e5b1773f01
David Ström <Dawid.Strom@gmail.com>
2018-03-09 12:04:49 +0100

    Changed map#getWeatherInfo route from /map/test to /map/getWeather.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'map/index'
+  get '/map/getWeather', to: 'map#getWeatherInfo'
   root 'map#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
